3747c8826ec48cb95462818c27b0344cab0951c9
ijarpa <ivanandres.jarpa@gmail.com>
2023-02-11 15:36:15 -0300

    Update

--- Project_League_of_legend/script/script.js
+++ Project_League_of_legend/script/script.js
@@ -1,8 +1,11 @@
-fetch('https://swapi.dev/api/starships')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+const API_URL = "https://swapi.dev/api/starships";
+
+const HTMLresponse = document.querySelector("#app");
+
+fetch(API_URL)
+    .then((response) => response.json())
+    .then((starships) => {
+        const tpl = starships.map((starship) => `<li>${starship.name}</li>`);
+        HTMLresponse.innerHTML = `<ul>${tpl.join('')}</ul>`;
+    });
+
