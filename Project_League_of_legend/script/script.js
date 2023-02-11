2e8514494b3db7d81dbe9bdfd534a1bfd05079e1
Ivan Jarpa <ivanandres.jarpa@gmail.com>
2023-02-11 10:44:22 -0300

    test API SW

---
+++ Project_League_of_legend/script/script.js
@@ -0,0 +1,8 @@
+fetch('https://swapi.dev/api/starships')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
